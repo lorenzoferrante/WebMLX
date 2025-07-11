31534b51fc29301127ac95ce9fb506b265e98b7a
Lorenzo Ferrante <lorenzoferrante96@gmail.com>
2025-07-11 11:51:41 +0200

    Initial Commit

---
+++ MLXTest/MLXTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLXTestApp.swift
+//  MLXTest
+//
+//  Created by Lorenzo Ferrante on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MLXTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
